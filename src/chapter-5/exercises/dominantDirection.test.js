74c6d145fa850d188e55240f1c35bd928ce31147
Christoph Matschiner <42239941+cmatschiner@users.noreply.github.com>
2019-01-07 18:14:28 +0100

    Testing CodeSandbox

--- src/chapter-5/exercises/dominantDirection.test.js
+++ src/chapter-5/exercises/dominantDirection.test.js
@@ -1,1 +1,11 @@
 import dominantDirection from "./dominantDirection";
+
+it("should return 'ltr' for string 'abcdef'", () => {
+  const actual = dominantDirection("abcdef");
+  expect(actual).toEqual("ltr");
+});
+
+it("should return 'ltr' for string ' مساء الخي'", () => {
+  const actual = dominantDirection(" مساء الخي");
+  expect(actual).toEqual("rtl");
+});
